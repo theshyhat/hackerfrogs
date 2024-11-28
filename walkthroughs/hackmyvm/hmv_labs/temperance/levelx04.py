28a9a94c45fdd4d6fcf73b8b6fd511d41a33adf9
HackerFrogs <115422975+theshyhat@users.noreply.github.com>
2024-11-27 19:28:09 -0500

    Update levelx04.py

--- walkthroughs/hackmyvm/hmv_labs/temperance/levelx04.py
+++ walkthroughs/hackmyvm/hmv_labs/temperance/levelx04.py
@@ -19,14 +19,21 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     data2 = s.recv(1024)
     print(data2)
 
-'''
+    # Convert the bytes to a string
+    challenge_string = data2.decode("utf-8")
+
+    # Reverse the string
+    reverse_string = challenge_string[::-1]
+    print(f"Reversed string\n{reverse_string}")
+
+    # Convert the string to bytes
+    answer_bytes = reverse_string.encode("utf-8")
 
     # Send the challenge solved / Envia el resultado del challenge.
-    print('Envio reto')
-    s.send(data2)
+    print('Sending answer...')
+    s.send(answer_bytes)
 
     # Receive the flag / Recibe la flag.
-    print('Recibo flag')
+    print('Receiving flag...')
     data3 = s.recv(1024)
     print(data3)
-'''
