83ccb5e33396b33e673e050c9d158710fd990766
HackerFrogs <115422975+theshyhat@users.noreply.github.com>
2025-05-15 00:34:59 -0700

    Create levelx02.py

---
+++ walkthroughs/hackmyvm/hmv_labs/temperance/levelx02.py
@@ -0,0 +1,42 @@
+import socket
+
+HOST = "temperance.hackmyvm.eu"
+PORT = 9988
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+
+    # Connect to the host and receive the initial message
+    print('Receiving Intro')
+    data = s.recv(1024)
+    print(data)
+
+    # Send "levelx02" to choose the level
+    current_level = 02
+    level_string = "levelx" + str(current_level)
+    level_bytes = level_string.encode("utf-8")
+    s.send(level_bytes)
+    print(f"Selecting level {current_level}")
+  
+    # Receive the challenge
+    print('Receiving challenge 1')
+    data2 = s.recv(1024)
+    print(data2)
+
+    # Send challenge 1 back
+    s.send(data2)
+    print(f"Sending challenge 1 back: {data2}")
+    
+    # Receive the second challenge
+    print('Receiving challenge 2')
+    data3 = s.recv(1024)
+    print(data3)
+  
+    # Send challenge 2 back
+    s.send(data3)
+    print(f"Sending challenge 2 back: {data3}")
+  
+    # Receive the flag
+    print('Receiving flag...')
+    data4 = s.recv(1024)
+    print(data4)
