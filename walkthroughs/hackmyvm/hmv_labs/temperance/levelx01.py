a638c626e96b2e075ca80ca7daea4ed25297aac6
HackerFrogs <115422975+theshyhat@users.noreply.github.com>
2025-05-16 16:30:20 -0700

    Update levelx01.py

--- walkthroughs/hackmyvm/hmv_labs/temperance/levelx01.py
+++ walkthroughs/hackmyvm/hmv_labs/temperance/levelx01.py
@@ -6,37 +6,33 @@ PORT = 9988
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
 
-    # Connect to the host and receive the initial message
+    # Connect to the host and receive the message
     print('Receiving Intro')
     data = s.recv(1024)
     print(data)
 
     # Send "levelx01" to choose the level
-    current_level = '01'
-    level_string = "levelx" + current_level
-    level_bytes = level_string.encode("utf-8")
-    s.send(level_bytes)
-    print(f"Selecting level {current_level}")
-  
-    # Receive the challenge
-    print('Receiving challenge 1')
+    s.send(b'levelx01')
+
+    # Receive the first challenge
+    print('Receiving first challenge.')
     data2 = s.recv(1024)
     print(data2)
 
-    # Send challenge 1 back
+    # Send the first challenge back
+    print('Sending first challenge.')
     s.send(data2)
-    print(f"Sending challenge 1 back: {data2}")
-    
+
     # Receive the second challenge
-    print('Receiving challenge 2')
+    print('Receiving second challenge.')
     data3 = s.recv(1024)
     print(data3)
-  
-    # Send challenge 2 back
+
+    # Send the second challenge back
+    print('Sending second challenge.')
     s.send(data3)
-    print(f"Sending challenge 2 back: {data3}")
-  
+
     # Receive the flag
-    print('Receiving flag...')
+    print('Receiving flag')
     data4 = s.recv(1024)
     print(data4)
