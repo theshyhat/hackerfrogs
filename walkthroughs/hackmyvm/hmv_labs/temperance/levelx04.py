23a9ad95f8a7700a8b12858a26d3c083cb60c752
HackerFrogs <115422975+theshyhat@users.noreply.github.com>
2025-05-23 16:35:39 -0700

    Update levelx04.py

--- walkthroughs/hackmyvm/hmv_labs/temperance/levelx04.py
+++ walkthroughs/hackmyvm/hmv_labs/temperance/levelx04.py
@@ -6,34 +6,36 @@ PORT = 9988
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
 
-    # Connect to the host and receive the message / Conecta al host y recibes la intro general.
+    # Connect to the host and receive the message
     print('Receiving Intro')
     data = s.recv(1024)
     print(data)
 
-    # Send "levelx04" to choose the level / Envia levelx00 para elegir el nivel.
+    # Send "levelx04" to choose the level
     s.send(b'levelx04')
 
-    # Receive the challenge / Recibe el challenge.
+    # Receive the challenge
     print('Receiving challenge.')
     data2 = s.recv(1024)
     print(data2)
 
-    # Convert the bytes to a string
-    challenge_string = data2.decode("utf-8")
-
+    # Convert the bytes into a string
+    data_string = data2.decode('utf-8')
+    print(f"This is the string:\n{data_string}")
+    
     # Reverse the string
-    reverse_string = challenge_string[::-1]
-    print(f"Reversed string\n{reverse_string}")
+    string_reverse = data_string[::-1]
+    print(f"This is the string in reverse:\n{string_reverse}")
 
-    # Convert the string to bytes
-    answer_bytes = reverse_string.encode("utf-8")
+    # Convert the reverse string into bytes
+    reverse_bytes = string_reverse.encode("utf-8")
+    print(f"These are the reverse bytes:\n{reverse_bytes}")
 
-    # Send the challenge solved / Envia el resultado del challenge.
-    print('Sending answer...')
-    s.send(answer_bytes)
+    # Send the challenge back
+    print('Sending challenge.')
+    s.send(reverse_bytes)
 
-    # Receive the flag / Recibe la flag.
-    print('Receiving flag...')
-    data3 = s.recv(1024)
-    print(data3)
+    # Receive the flag
+    print('Receiving flag')
+    data4 = s.recv(1024)
+    print(data4)
