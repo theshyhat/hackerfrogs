47f9265c2742439bf4608445d2c3698a1ee4b93d
HackerFrogs <115422975+theshyhat@users.noreply.github.com>
2025-05-23 16:50:39 -0700

    Update levelx05.py

--- walkthroughs/hackmyvm/hmv_labs/temperance/levelx05.py
+++ walkthroughs/hackmyvm/hmv_labs/temperance/levelx05.py
@@ -6,34 +6,36 @@ PORT = 9988
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
 
-    # Connect to the host and receive the message / Conecta al host y recibes la intro general.
+    # Connect to the host and receive the message
     print('Receiving Intro')
     data = s.recv(1024)
     print(data)
 
-    # Send "levelx05" to choose the level / Envia levelx00 para elegir el nivel.
+    # Send "levelx05" to choose the level
     s.send(b'levelx05')
 
-    # Receive the challenge / Recibe el challenge.
+    # Receive the challenge
     print('Receiving challenge.')
     data2 = s.recv(1024)
     print(data2)
 
-    # Convert the bytes to a string
-    challenge_string = data2.decode("utf-8")
+    # Convert the bytes into a string
+    data_string = data2.decode('utf-8')
+    print(f"This is the string:\n{data_string}")
+    
+    # Grab only the last 5 characters of the string
+    last5_string = data_string[-5:]
+    print(f"These are the last 5 characters:\n{last5_string}")
 
-    # Retrieve the last 5 characters of the string
-    last_5_string = challenge_string[-5::]
-    print(f"Last 5 characters\n{last_5_string}")
+    # Convert the string into bytes
+    last5_bytes = last5_string.encode("utf-8")
+    print(f"These are the last 5 bytes:\n{last5_bytes}")
 
-    # Convert the string to bytes
-    answer_bytes = last_5_string.encode("utf-8")
+    # Send the challenge back
+    print('Sending challenge.')
+    s.send(last5_bytes)
 
-    # Send the challenge solved / Envia el resultado del challenge.
-    print('Sending answer...')
-    s.send(answer_bytes)
-
-    # Receive the flag / Recibe la flag.
-    print('Receiving flag...')
-    data3 = s.recv(1024)
-    print(data3)
+    # Receive the flag
+    print('Receiving flag')
+    data4 = s.recv(1024)
+    print(data4)
