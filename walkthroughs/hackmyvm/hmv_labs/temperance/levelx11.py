b4f3249654f7866c16608e4f0977fa8b583c0651
HackerFrogs <115422975+theshyhat@users.noreply.github.com>
2025-06-20 16:33:06 -0700

    Create levelx11.py

---
+++ walkthroughs/hackmyvm/hmv_labs/temperance/levelx11.py
@@ -0,0 +1,95 @@
+import socket
+
+HOST = "temperance.hackmyvm.eu"
+PORT = 9988
+
+MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
+                    'C':'-.-.', 'D':'-..', 'E':'.',
+                    'F':'..-.', 'G':'--.', 'H':'....',
+                    'I':'..', 'J':'.---', 'K':'-.-',
+                    'L':'.-..', 'M':'--', 'N':'-.',
+                    'O':'---', 'P':'.--.', 'Q':'--.-',
+                    'R':'.-.', 'S':'...', 'T':'-',
+                    'U':'..-', 'V':'...-', 'W':'.--',
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+                    '1':'.----', '2':'..---', '3':'...--',
+                    '4':'....-', '5':'.....', '6':'-....',
+                    '7':'--...', '8':'---..', '9':'----.',
+                    '0':'-----', ', ':'--..--', '.':'.-.-.-',
+                    '?':'..--..', '/':'-..-.', '-':'-....-',
+                    '(':'-.--.', ')':'-.--.-'}
+
+def decrypt(message):
+
+    # extra space added at the end to access the
+    # last morse code
+    message += ' '
+
+    decipher = ''
+    citext = ''
+    for letter in message:
+
+        # checks for space
+        if (letter != ' '):
+
+            # counter to keep track of space
+            i = 0
+
+            # storing morse code of a single character
+            citext += letter
+
+        # in case of space
+        else:
+            # if i = 1 that indicates a new character
+            i += 1
+
+            # if i = 2 that indicates a new word
+            if i == 2 :
+
+                 # adding space to separate words
+                decipher += ' '
+            else:
+
+                # accessing the keys using their values (reverse of encryption)
+                decipher += list(MORSE_CODE_DICT.keys())[list(MORSE_CODE_DICT
+                .values()).index(citext)]
+                citext = ''
+
+    return decipher
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+
+    # Connect to the host and receive the message
+    print('Receiving Intro')
+    data = s.recv(1024)
+    print(data)
+
+    # Send "levelx11" to choose the level
+    s.send(b'levelx11')
+
+    # Receive the challenge
+    print('Receiving challenge.')
+    data2 = s.recv(1024)
+    print(data2)
+
+    # Convert the bytes into a string
+    data_string = data2.decode('utf-8')
+    print(f"These are the bytes converted into a string:\n{data_string}")
+
+    # Run the decrypt function on the Morse code
+    decrypted_morse_code = decrypt(data_string)
+    print(f"This is the message decoded from Morse code:\n{decrypted_morse_code}")
+
+    # Convert the string to bytes
+    bytes = decrypted_morse_code.encode('utf-8')
+    print(f"This is the string converted to bytes:\n{bytes}")
+
+    # Send the challenge back
+    print('Sending challenge.')
+    s.send(bytes)
+
+    # Receive the flag
+    print('Receiving flag')
+    data4 = s.recv(1024)
+    print(data4)
