d80f97ff3cfe14da67b6021edf565850dd68291a
HackerFrogs <115422975+theshyhat@users.noreply.github.com>
2025-06-27 16:16:06 -0700

    Update levelx13.py

--- walkthroughs/hackmyvm/hmv_labs/temperance/levelx13.py
+++ walkthroughs/hackmyvm/hmv_labs/temperance/levelx13.py
@@ -23,24 +23,24 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     data_string = data2.decode('utf-8')
     print(f"These are the bytes converted into a string:\n{data_string}")
 
-    # Remove the brackets from the string
-    string_mod = data_string[1:-1]
-    print(f"This is the string without the brackets:\n{string_mod}")
+    # Removing the left and right square bracket from the string
+    mod_string = data_string[1:-1]
+    print(f"This is the modified string, without the brackets:\n{mod_string}")
 
-    # Convert the string into a list
-    list_from_string = string_mod.split() 
-    print(f"This is the string separated into a list:\n{list_from_string}")
+    # Convert the string in a list
+    string_list = mod_string.split()
+    print(f"This is the string converted to a list:\n{string_list}")
 
-    # Sort the list
-    list_from_string.sort()
-    print(f"This is the sorted list:\n{list_from_string}")
+    # Sorting the list alphabetically
+    string_list.sort()
+    print(f"This is the sorted list:\n{string_list}")
 
-    # Retrieve the last element of the list
-    list_last_item = list_from_string[-1]
-    print(f"This is the last item in the list:\n{list_last_item}")
+    # Retrieve the last item in the list
+    last_item = string_list[-1]
+    print(f"This is the last item in the list:\n{last_item}")
 
     # Convert the string to bytes
-    bytes = list_last_item.encode('utf-8')
+    bytes = last_item.encode('utf-8')
     print(f"This is the string converted to bytes:\n{bytes}")
 
     # Send the challenge back
