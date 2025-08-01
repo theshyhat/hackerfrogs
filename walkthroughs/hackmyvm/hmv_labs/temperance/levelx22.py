9a6f0395d80c8d1c59b653fa1f57626fff103e3c
HackerFrogs <115422975+theshyhat@users.noreply.github.com>
2025-08-01 16:42:22 -0700

    Update levelx22.py

--- walkthroughs/hackmyvm/hmv_labs/temperance/levelx22.py
+++ walkthroughs/hackmyvm/hmv_labs/temperance/levelx22.py
@@ -3,52 +3,50 @@ import socket
 HOST = "temperance.hackmyvm.eu"
 PORT = 9988
 
-# Write decimal list to ASCII function
-def decimals_to_ascii(decimal_list):
-    ascii_string = ''
-    for num in decimal_list:
-        try:
-            ascii_string += chr(num)
-        except ValueError:
-            print(f"Skipping invalid ASCII value: {num}")
-    return ascii_string
-
-# Connection code
-
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
 
-    # Connect to the host and receive the message / Conecta al host y recibes la intro general.
+    # Connect to the host and receive the message
     print('Receiving Intro')
     data = s.recv(1024)
     print(data)
 
-    # Send "levelx22" to choose the level / Envia levelx00 para elegir el nivel.
+    # Send "levelx22" to choose the level
     s.send(b'levelx22')
 
-    # Receive the challenge / Recibe el challenge.
-    print('Receiving challenge...')
+    # Receive the challenge
+    print('Receiving challenge.')
     data2 = s.recv(1024)
     print(data2)
 
-    # Convert data2 to a string
-    ascii_decimal_string = data2.decode('utf-8')
+    # Save the data2 variable as an string
+    data_string = data2.decode('utf-8')
+    print(f"These are the bytes converted to a string:\n{data_string}")
 
-    # Convert the string to a list 
-    ascii_decimal_list = [int(num) for num in ascii_decimal_string.split()]
+    # Convert the string into a list
+    data_list = data_string.split()
+    print(f"These is the string converted to a list:\n{data_list}")
 
-    # Create an empty string to be populated in the next step
-    ascii_string = decimals_to_ascii(ascii_decimal_list)
-    print(f"This is the ASCII string {ascii_string}")
+    # Do a for loop which converts the elements in the list into ASCII characters
+    def convert_dec_to_ascii(target_list):
+        ascii_string = ''
+        for i in target_list:
+            ascii_string += chr(int(i))
+        return ascii_string
 
-    # Convert the string to bytes
-    ascii_bytes = ascii_string.encode('utf-8')
+    # Call the function
+    converted_string = convert_dec_to_ascii(data_list)
+    print(f"These are the decimal numbers converted to ASCII:\n{converted_string}")
 
-    # Send the challenge solved / Envia el resultado del challenge.
-    print('Sending response...')
-    s.send(ascii_bytes)
+    # Convert the string into bytes
+    bytes = converted_string.encode('utf-8')
+    print(f"This is the string converted to bytes:\n{bytes}")
 
-    # Receive the flag / Recibe la flag.
-    print('Receiving flag...')
-    data3 = s.recv(1024)
-    print(data3)
+    # Send the challenge back
+    print('Sending challenge.')
+    s.send(bytes)
+
+    # Receive the flag
+    print('Receiving flag')
+    data4 = s.recv(1024)
+    print(data4)
